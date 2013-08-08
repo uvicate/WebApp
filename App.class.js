08ae46f9cf6ad1b72e492ac251223012b1cfd8d8
victorhqc <victorhqc@gmail.com>
2013-08-08 13:38:35 -0600

    Added pruned code

--- App.class.js
+++ App.class.js
@@ -233,7 +233,7 @@ function Initclass(j){
         if(this.modo === 'POST' && this.enviaArchivo === false){
             var datos = '';
             for(var llave in this.datos){
-            	if(typeof this.datos[llave] == "object"){
+            	if(typeof this.datos[llave] === "object"){
                 	this.datos[llave] = JSON.pruned(this.datos[llave]);
             	}
                 datos +=encodeURIComponent(llave)+'='+encodeURIComponent(this.datos[llave])+'&';
@@ -257,8 +257,23 @@ function Initclass(j){
             if(http.readyState === 4){
                 if(http.status === 200){
                     esto.success(callback);
+                }else{
+                	switch(http.status){
+                		case 500:
+                		case 404:
+                		case 413:
+                			esto.failed(callback, http.status);
+                		break;
+                	}
                 }
             } 
+        }
+    }
+
+    _Vi.prototype.failed = function(callback, status){
+    	if(typeof callback === 'function'){
+    		r = {success: false, status:status};
+        	callback(r);
         }
     }
 
@@ -356,7 +371,11 @@ function Initclass(j){
     }
 
     _Vi.prototype.DOMHandler = function(){
-    	var dom = document.querySelector(this.div);
+    	if(typeof this.div !== 'object' && this.div.appendChild === undefined){
+    		var dom = document.querySelector(this.div);
+    	}else{
+    		var dom = this.div
+    	}
     	this.dom = dom;
     }
 
@@ -1130,3 +1149,106 @@ function Initclass(j){
 		return new _Lang(json);
 	}
 })();
+
+// JSON.pruned : a function to stringify any object without overflow
+// example : var json = JSON.pruned({a:'e', c:[1,2,{d:{e:42, f:'deep'}}]})
+// two additional optional parameters :
+//   - the maximal depth (default : 6)
+//   - the maximal length of arrays (default : 50)
+// GitHub : https://github.com/Canop/JSON.prune
+// This is based on Douglas Crockford's code ( https://github.com/douglascrockford/JSON-js/blob/master/json2.js )
+(function () {
+    'use strict';
+
+    var DEFAULT_MAX_DEPTH = 1000;
+    var DEFAULT_ARRAY_MAX_LENGTH = 1000;
+    var seen; // Same variable used for all stringifications
+
+    Date.prototype.toPrunedJSON = Date.prototype.toJSON;
+    String.prototype.toPrunedJSON = String.prototype.toJSON;
+
+    var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+        escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+        meta = {    // table of character substitutions
+            '\b': '\\b',
+            '\t': '\\t',
+            '\n': '\\n',
+            '\f': '\\f',
+            '\r': '\\r',
+            '"' : '\\"',
+            '\\': '\\\\'
+        };
+
+    function quote(string) {
+        escapable.lastIndex = 0;
+        return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
+            var c = meta[a];
+            return typeof c === 'string'
+                ? c
+                : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+        }) + '"' : '"' + string + '"';
+    }
+
+    function str(key, holder, depthDecr, arrayMaxLength) {
+        var i,          // The loop counter.
+            k,          // The member key.
+            v,          // The member value.
+            length,
+            partial,
+            value = holder[key];
+        if (value && typeof value === 'object' && typeof value.toPrunedJSON === 'function') {
+            value = value.toPrunedJSON(key);
+        }
+
+        switch (typeof value) {
+        case 'string':
+            return quote(value);
+        case 'number':
+            return isFinite(value) ? String(value) : 'null';
+        case 'boolean':
+        case 'null':
+            return String(value);
+        case 'object':
+            if (!value) {
+                return 'null';
+            }
+            if (depthDecr<=0 || seen.indexOf(value)!==-1) {
+                return '"-pruned-"';
+            }
+            seen.push(value);
+            partial = [];
+            if (Object.prototype.toString.apply(value) === '[object Array]') {
+                length = Math.min(value.length, arrayMaxLength);
+                for (i = 0; i < length; i += 1) {
+                    partial[i] = str(i, value, depthDecr-1, arrayMaxLength) || 'null';
+                }
+                v = partial.length === 0
+                    ? '[]'
+                    : '[' + partial.join(',') + ']';
+                return v;
+            }
+            for (k in value) {
+                if (Object.prototype.hasOwnProperty.call(value, k)) {
+                    try {
+                        v = str(k, value, depthDecr-1, arrayMaxLength);
+                        if (v) partial.push(quote(k) + ':' + v);
+                    } catch (e) { 
+                        // this try/catch due to some "Accessing selectionEnd on an input element that cannot have a selection." on Chrome
+                    }
+                }
+            }
+            v = partial.length === 0
+                ? '{}'
+                : '{' + partial.join(',') + '}';
+            return v;
+        }
+    }
+
+    JSON.pruned = function (value, depthDecr, arrayMaxLength) {
+        seen = [];
+        depthDecr = depthDecr || DEFAULT_MAX_DEPTH;
+        arrayMaxLength = arrayMaxLength || DEFAULT_ARRAY_MAX_LENGTH;
+        return str('', {'': value}, depthDecr, arrayMaxLength);
+    };
+
+}());
